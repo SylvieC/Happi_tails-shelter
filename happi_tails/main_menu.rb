7a8627aa43009fdb3bb8c6a425d9c825fdfee021
SylvieC <sylviecottrell@gmail.com>
2014-01-12 09:04:05 -0800

    added some optios

--- happi_tails/main_menu.rb
+++ happi_tails/main_menu.rb
@@ -1,10 +1,9 @@
 require './shelter'
 require './animal'
 require './client'
-require 'pry'
 
 
-  shelter = Shelter.new("Happy Tails")
+shelter = Shelter.new("Happy Tails")
 
 def menu (message)
   puts `clear`
@@ -30,8 +29,12 @@ while choice != 'q'
   case choice
   when "1"
     # 1) display all animals
-    puts "The animals presently in #{shelter.name} are: "
-    shelter.display_animals
+    if shelter.animals.length == 0
+      puts " No animals at this moment"
+    else  
+      puts "The animals presently in #{shelter.name} are: "
+      shelter.display_animals
+    end  
   when "2"
     message += 'option 2'
     # 1) diplay all clients
@@ -39,7 +42,7 @@ while choice != 'q'
     shelter.display_clients
   when "3"
     message += 'option 3'
-    # create an animal and put it in the shelter
+    # create an animal 
     #1) prompt user for name, age, gender, species, toys 
     puts "Animal's name: "
     name = gets.chomp
@@ -61,13 +64,11 @@ while choice != 'q'
        continue = false
       end
     end
-    #2)create an animal ( this animal's characteristics will be added in the array animals_created
-     #  when I initialize it )
+    #2)create an animal and add it to the shelter with the shelter's 'receive_animal' method
     new_animal = Animal.new(name, age, gender, species, toys)
     shelter.receive_animal(new_animal)
     puts "A #{new_animal.species} named #{new_animal.name} was added to our "\
          "#{shelter.name} shelter"
-
   when "4"
     message += 'option 4'
     # Create a client
@@ -80,17 +81,15 @@ while choice != 'q'
     age = gets.chomp
     puts  " number of pets:"
     num_of_pets = gets.chomp
-    #2)create a client and add to the list of clients of the shelter
+    #2)create a client and add to the list of clients of the shelter with method add_client
     new_client = Client.new(name, num_of_children,age, num_of_pets)
     shelter.add_client(new_client)
     puts " A new client named #{new_client.name} was added to our list of clients "
-    
   when "5"
     message += 'option 5'
     # facilitate client adopt an animal
-
     #1)Show the animals ready for adoption
-     if shelter.animals.length == 0
+    if shelter.animals.length == 0
       puts "Sorry, all our animals have been adopted"
     else
      puts "Animals ready for adoption: "
@@ -99,52 +98,53 @@ while choice != 'q'
      puts "Choose the name of the animal to be adopted."
      puts " Name:"
      chosen_animal_name = gets.chomp
-     puts "species"
+     puts "species:"
      chosen_animal_species = gets.chomp
      chosen_animal = shelter.animals.select do|animal| 
-        animal.name == chosen_animal_name && animal.species == chosen_animal_species
-      end
-      chosen_animal = chosen_animal.first 
-      puts "You chose a #{chosen_animal.species} named #{chosen_animal.name}"
+         animal.name == chosen_animal_name && animal.species == chosen_animal_species
+     end
+     chosen_animal = chosen_animal.first 
+     puts "You chose #{chosen_animal.name}"
     #2)Show the clients
      puts "List of possible clients who could adopt #{chosen_animal_name}:"
      shelter.display_clients()
-     #? I am not sure how to decide which client is willing to adopt
-    #4)Pick a client to adopt the animal
+    #4)User picks a client to adopt the animal
      puts "Choose a client to adopt #{chosen_animal.name}"
      name_of_client_who_adopts = gets.chomp
      client_who_adopts = shelter.clients.select do |client| 
          client.name == name_of_client_who_adopts
        end
-       #in case there are two clients with the same name, I choose the first one
-      puts "Getting ready to finalize the adoption process of our #{chosen_animal.name}"
-    #5) Make the adoption happen (the client gains a pet and the shelter gives it for adoption)
-     client_who_adopts.first.adopt()
+     client_who_adopts = client_who_adopts.first
+     puts "Getting ready to finalize the adoption process of #{chosen_animal.name}..."
+    #5) Make the adoption happen (the client adopts, then the shelter givers theshelter and the shelt
+    #   shelter gives that animal away
+     client_who_adopts.adopt()
      shelter.give_for_adoption(chosen_animal)
-     puts "Everything worked perfectly, #{client_who_adopts.first.name} has successfully "\
+     puts  "Everything worked perfectly, #{client_who_adopts.name} has successfully "\
           "adopted #{chosen_animal.name} "
     end
   when "6"
     message += 'option 6'
     # facilitate client puts animal up for adoption
     #1)Ask if a client wants to give an animal for adoption
-      puts "A client wants to give an animal for adoption?(y or n)"
-      continue = gets.chomp
+    puts "A client wants to give an animal for adoption?(y or n)"
+    continue = gets.chomp
     #2) If yes, ask for the client's info and for the animal's info
-      if continue == "y"
-        puts "Name of the client giving an animal for adoption:"
-        name_of_client_giving_up_animal = gets.chomp
-        giver = shelter.clients.select { |client| client.name == name_of_client_giving_up_animal}
-          if giver == []
-            puts "We don't have a client named #{name_of_client_giving_up_animal}"
-            puts "Please select option 4 if you want to add this person to our clients' list.\n Then come back ot option 6"  
-          elsif giver.length > 1
-            puts "please enter the client's age, because the shelter has several clients"
-                 "named #{giver.name}:"
-            age = gets.chomp
-            giver = giver.select { |client| client.age == age}.first
-          else 
-          
+    if continue == "y"
+      puts "Name of the client giving an animal for adoption:"
+      name_of_client_giving_up_animal = gets.chomp
+      giver = shelter.clients.select { |client| client.name == name_of_client_giving_up_animal}
+      if giver == []
+        puts "We don't have a client named #{name_of_client_giving_up_animal}"
+        puts "To accept the animal, #{name_of_client_giving_up_animal} needs to be "\
+             "added to our clients' list first. To do so, select 'Create a client' (option 4)."
+        puts "Then come back to the option 'facilitate client puts animal for adoption'"  
+      elsif giver.length > 1
+        puts "please enter the client's age, because the shelter has several clients"
+             "named #{giver.name}:"
+        age = gets.chomp
+        giver = giver.select { |client| client.age == age}.first
+      else 
         puts " Information about the animal given for adoption:"
         puts "name:"
         name = gets.chomp
@@ -167,11 +167,11 @@ while choice != 'q'
         puts " Process successful:"
         puts " #{new_animal.name} is now in our #{shelter.name} shelter"
       end
-     elsif continue == "n"
+    elsif continue == "n"
         puts "This option doesn't apply since no client is ready for adoption now" 
-     else 
+    else 
       puts "The answer has to be yes or no ( y or n), please try again later" 
-     end
+    end
    
 
 else
